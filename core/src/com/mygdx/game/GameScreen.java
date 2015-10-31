22cd10239464d390faa9893b73874fd1fe626614
Johannes Sjölund <j.sjolund@gmail.com>
2015-10-31 19:46:55 +0100

    Added a layer/house level slider. Fixed some actor listener issues.

--- core/src/com/mygdx/game/GameScreen.java
+++ core/src/com/mygdx/game/GameScreen.java
@@ -154,6 +154,8 @@ public class GameScreen implements Screen {
 	@Override
 	public void render(float delta) {
 		camera.update(delta, GameSettings.CAMERA_LERP_ALPHA);
+		stage.act(delta);
+
 		delta *= GameSettings.GAME_SPEED;
 		Gdx.gl.glClearColor(0, 0, 0, 1f);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT | GL20.GL_DEPTH_BUFFER_BIT);
@@ -178,7 +180,6 @@ public class GameScreen implements Screen {
 			debugDraw.setDebugMode(mode);
 			engine.debugDrawWorld(camera);
 		}
-		stage.act(delta);
 		stage.draw();
 
 	}
