49b2eabb8df34a720f4ab45e0fdd7befb1f8df70
Johannes Sjölund <j.sjolund@gmail.com>
2016-03-25 14:28:32 +0100

    Fix problem with model scaling when getting armature position

--- core/src/com/mygdx/game/objects/GameCharacter.java
+++ core/src/com/mygdx/game/objects/GameCharacter.java
@@ -18,6 +18,7 @@ package com.mygdx.game.objects;
 
 import com.badlogic.gdx.graphics.g3d.Model;
 import com.badlogic.gdx.graphics.g3d.model.Node;
+import com.badlogic.gdx.math.Matrix4;
 import com.badlogic.gdx.math.Quaternion;
 import com.badlogic.gdx.math.Vector3;
 import com.badlogic.gdx.physics.bullet.collision.btCollisionShape;
@@ -42,13 +43,14 @@ public abstract class GameCharacter extends SteerableBody {
 		selectionMarkerOffset = new Vector3(0, -halfExtents.y * 0.95f, 0);
 	}
 
-	private final Vector3 tmp1 = new Vector3();
-	private final Vector3 tmp2 = new Vector3();
-	private final Vector3 tmp3 = new Vector3();
-	private final Quaternion tmpModelRot = new Quaternion();
+	private final static Vector3 TMP_V1 = new Vector3();
+	private final static Vector3 TMP_V2 = new Vector3();
+	private final static Vector3 TMP_V3 = new Vector3();
+	private final static Quaternion TMP_Q = new Quaternion();
+	private final static Matrix4 TMP_M = new Matrix4();
 
 	public float getBoneOrientation(String nodeId) {
-		return BulletSteeringUtils.vectorToAngle(getBoneDirection(nodeId, tmp3));
+		return BulletSteeringUtils.vectorToAngle(getBoneDirection(nodeId, TMP_V3));
 	}
 
 	/**
@@ -61,12 +63,12 @@ public abstract class GameCharacter extends SteerableBody {
 	public Vector3 getBoneDirection(String nodeId, Vector3 out) {
 		Node node = modelInstance.getNode(nodeId);
 		Node endPointNode = (node.hasChildren()) ? node.getChild(0) : node;
-		node.globalTransform.getTranslation(tmp1);
-		endPointNode.globalTransform.getTranslation(tmp2);
-		tmp1.sub(tmp2).scl(-1);
-		modelInstance.transform.getRotation(tmpModelRot);
-		tmpModelRot.transform(tmp1);
-		return out.set(tmp1).nor();
+		node.globalTransform.getTranslation(TMP_V1);
+		endPointNode.globalTransform.getTranslation(TMP_V2);
+		TMP_V1.sub(TMP_V2).scl(-1);
+		modelInstance.transform.getRotation(TMP_Q);
+		TMP_Q.transform(TMP_V1);
+		return out.set(TMP_V1).nor();
 	}
 
 	/**
@@ -79,12 +81,13 @@ public abstract class GameCharacter extends SteerableBody {
 	public Vector3 getBoneMidpointWorldPosition(String nodeId, Vector3 out) {
 		Node node = modelInstance.getNode(nodeId);
 		Node endPointNode = (node.hasChildren()) ? node.getChild(0) : node;
-		endPointNode.globalTransform.getTranslation(tmp1);
-		endPointNode.localTransform.getTranslation(tmp2).scl(0.5f);
-		tmp1.add(tmp2);
-		modelInstance.transform.getRotation(tmpModelRot);
-		tmpModelRot.transform(tmp1);
-		tmp1.add(getPosition());
-		return out.set(tmp1);
+		// Use global transform to account for model scaling
+		node.globalTransform.getTranslation(TMP_V1);
+		TMP_V3.set(TMP_V1);
+		endPointNode.globalTransform.getTranslation(TMP_V2);
+		TMP_V3.sub(TMP_V1.sub(TMP_V2).scl(0.5f));
+		modelInstance.transform.getRotation(TMP_Q, true).transform(TMP_V3);
+		TMP_V3.add(getPosition());
+		return out.set(TMP_V3);
 	}
 }
